83d620e1cd856d8f44cd156c3235f7f49e498132
Mr penington <benjaminbkris@gmail.com>
2026-08-20 14:02:52 +0200

    Update route.ts

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -1,12 +1,18 @@
 import { NextResponse } from 'next/server';
 
+const dbConnectionString = process.env.DATABASE_URL; 
+
 // Dette er ren Node.js som kjører på server-siden (backend)
 export async function GET() {
-  const backendData = {
-    tittel: "Hilsen fra Node.js!",
-    status: "Suksess",
-    tidspunkt: new Date().toLocaleTimeString('nb-NO')
-  };
 
-  return NextResponse.json(backendData);
+    
+    
+    const backendData = {
+        tittel: "Hilsen fra Node.js!",
+        status: "Suksess",
+        tidspunkt: new Date().toLocaleTimeString('nb-NO'),
+        db_connected: dbConnectionString ? "Yes" : "No"
+    };
+
+    return NextResponse.json(backendData);
 }
