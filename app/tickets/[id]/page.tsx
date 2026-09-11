1dca3657cf51a04678c2e7ada79c064a50fd4c60
Mr penington <benjaminbkris@gmail.com>
2026-09-11 13:19:10 +0200

    Update page.tsx

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1226,7 +1226,7 @@ async function sendMessage() {
 
             <section className="pt-10 rounded-xl border border-slate-200 bg-white shadow-sm dark:border-slate-800 dark:bg-slate-900">
               <div>
-                <h1 className="text-lg font-bold text-slate-900 dark:text-white mb-4 flex items-center justify-center gap-2">
+                <h1 className="text-3xl text-lg font-bold text-slate-900 dark:text-white mb-4 flex items-center justify-center gap-2">
                   {ticket.title}
                 </h1>
               </div>
