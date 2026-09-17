77ac94d7ff62ec3e7f78e44096cca629b80cbadf
Mr penington <benjaminbkris@gmail.com>
2026-09-17 13:54:38 +0200

    Update page.tsx

--- app/admin/tickets/page.tsx
+++ app/admin/tickets/page.tsx
@@ -1321,6 +1321,23 @@ function TicketRow({
     };
   }, []);
 
+
+  // ==================================================
+  // CLOSE DROPDOWN WHEN SCROLLING
+  // ==================================================
+
+  useEffect(() => {
+    function handleScroll() {
+        setAssignmentOpen(false);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+        window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   // ==================================================
   // UPDATE DROPDOWN POSITION
   // ==================================================
@@ -1791,7 +1808,7 @@ function TicketRow({
           <button
             type="button"
             onClick={onOpen}
-            className="w-full cursor-pointer rounded-lg border border-slate-200 bg-white px-3 py-2 text-xs font-semibold text-slate-700 transition hover:border-blue-300 hover:bg-blue-50 hover:text-blue-700 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-200 dark:hover:border-blue-800 dark:hover:bg-blue-950/30 dark:hover:text-blue-400"
+            className="whitespace-nowrap w-full cursor-pointer rounded-lg border border-slate-200 bg-white px-3 py-2 text-xs font-semibold text-slate-700 transition hover:border-blue-300 hover:bg-blue-50 hover:text-blue-700 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-200 dark:hover:border-blue-800 dark:hover:bg-blue-950/30 dark:hover:text-blue-400"
           >
             Åpne sak →
           </button>
